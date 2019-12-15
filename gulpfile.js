95a2fa2e2002c1bd32e13b6033afe8311cd39721
Natalya <scheikina.nat@ya.ru>
2019-12-15 21:20:16 +0300

    Автоматизирует (генерация css и вотчер)

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function(done) {
+  gulp.src("less/style.less")
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+    done();
+});
+
+gulp.task("serve", function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  })
+
+  gulp.watch("less/**/*.less", gulp.series("style"))
+  gulp.watch("*.html").on("change", server.reload)
+
+});
